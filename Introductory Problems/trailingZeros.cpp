7203c7bd9fd86a051cd9cd4cef3d009e1e992d91
ritikajagtap <ritika.jagtap.2002@gmail.com>
2024-03-29 12:51:29 +0530

    trailing zeroes

---
+++ Introductory Problems/trailingZeros.cpp
@@ -0,0 +1,37 @@
+/* Jai Shree Krishna */
+/* Ritika Jagtap */
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+long long gcd(long long int a, long long int b);
+long long lcm(int a, int b);
+const long long int MOD = 1e9 + 7;
+
+int main()
+{
+    long long int n;
+    cin>>n;
+    long long int cnt = 0;
+    long long int current = 5;
+    while(current<=n){
+        cnt += n/current;
+        current *= 5;
+    }
+    cout<<cnt<<endl;
+}
+
+void solve()
+{
+    
+}
+long long gcd(long long int a, long long int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+long long lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
